c7bddf4b182686ec3c28ca8fd8dabeba3a341636
Notes Editor <notes-editor@server>
2026-02-08 08:32:29 +0100

    Use prod.local and port fallbacks for Android API

--- app/android/app/src/main/java/com/bartmann/noteseditor/AppConfig.kt
+++ app/android/app/src/main/java/com/bartmann/noteseditor/AppConfig.kt
@@ -3,7 +3,11 @@ package com.bartmann.noteseditor
 object AppConfig {
     const val AUTH_TOKEN = "J1tYYSyLhZXrcdq05GnUNHXtGg/Xi/pUeNQ+NbtW4sk"
     val BASE_URLS = listOf(
+        "http://prod.local",
+        "http://prod.local:8080",
+        "http://192.168.1.27",
         "http://192.168.1.27:8080",
+        "http://100.87.83.30",
         "http://100.87.83.30:8080"
     )
 }
