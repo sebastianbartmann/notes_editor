e65569271064213aea4135a32930bfe06ccc594c
Notes Editor <notes-editor@server>
2026-02-12 15:59:42 +0100

    Widen files view editor area in web app

--- clients/web/src/components/Layout/Layout.tsx
+++ clients/web/src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import { useLocation } from 'react-router-dom'
 import Header from './Header'
 import Navigation from './Navigation'
 import styles from './Layout.module.css'
@@ -8,12 +9,15 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const location = useLocation()
+  const isFilesRoute = location.pathname === '/files' || location.pathname.startsWith('/files/')
+
   return (
     <div className={styles.layout}>
       <Header />
       <Navigation />
       <main className={styles.main}>
-        <div className={styles.container}>
+        <div className={`${styles.container} ${isFilesRoute ? styles.containerWide : ''}`}>
           {children}
         </div>
       </main>
